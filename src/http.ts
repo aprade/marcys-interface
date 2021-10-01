765a00a31a7db9460a3e548b88905bc15bbaad66
Joao Koritar <jakoritarleite@gmail.com>
2021-10-01 17:27:58 -0300

    fix: axios wasn't working properly so I've rolled back to node-fetch

--- src/http.ts
+++ src/http.ts
@@ -1,87 +1,34 @@
-import axios, { AxiosRequestConfig } from 'axios';
-
 enum Methods {
-  POST = 'post',
-  GET = 'get'
+  POST = 'POST',
+  GET = 'GET'
 }
 
-interface Config<T> {
+interface Init<T> {
   method?: Methods;
-  data?: T;
+  body?: T;
 }
 
-const request = <T>(
-  endpoint: string,
-  config: Config<T>
-): AxiosRequestConfig => {
-  const url = process.env.API_URL;
-
+const request = <T>(init: Init<T>): RequestInit => {
   return {
-    url: `${url}/${endpoint}`,
     headers: {
       'Content-Type': 'application/json'
     },
-    ...config
+    method: init.method || 'GET',
+    body: init.body ? JSON.stringify(init.body) : null
   };
 };
 
-const http = async <T>(req: AxiosRequestConfig): Promise<T> => {
-  const res = await axios(req);
-  console.log('http.http.res', res);
-  const body = res.data as T;
-  console.log('http.http.body', body);
+const http = async <T>(endpoint: string, req: RequestInit): Promise<T> => {
+  const url = process.env.API_URL;
+
+  const res = await fetch(`${url}/${endpoint}`, req);
+  const body = (await res.json()) as T;
 
   return body;
 };
 
 export const get = async <T>(endpoint: string): Promise<T> =>
-  http<T>(request(endpoint, { method: Methods.GET }));
+  http<T>(endpoint, request({ method: Methods.GET }));
 
 export const post = async <I, D>(endpoint: string, body: I): Promise<D> =>
-  http<D>(request<I>(endpoint, { method: Methods.POST, data: body }));
-
-// import fetch, { Request, RequestInfo } from 'node-fetch';
-
-// interface Init {
-//   method?: string;
-//   body?: string;
-// }
-
-// const request = (endpoint: string, init?: Init): Request => {
-//   const url = process.env.API_URL || 'http://localhost:3000/';
-
-//   return new Request(`${url}/${endpoint}`, {
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     ...init
-//   });
-// };
-
-// const http = async <T>(req: RequestInfo): Promise<T> => {
-//   const res = await fetch(req);
-//   const body = (await res.json()) as T;
-
-//   // For non-success status codes we throw the body as it carries the error type.
-//   if (!res.ok) {
-//     throw new Error(`${res} \n${body}`);
-//   }
-
-//   return body;
-// };
-
-// export const get = async <T>(endpoint: string, options?: any): Promise<T> =>
-//   http<T>(request(endpoint, { method: 'GET', ...options }));
-
-// export const post = async <I, D>(
-//   endpoint: string,
-//   body: I,
-//   options?: any
-// ): Promise<D> =>
-//   http<D>(
-//     request(endpoint, {
-//       method: 'POST',
-//       body: JSON.stringify(body),
-//       ...options
-//     })
-//   );
+  http<D>(endpoint, request<I>({ method: Methods.POST, body: body }));
